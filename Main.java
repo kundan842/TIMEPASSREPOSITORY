cdd0e329690b962cc8667c32636e1c6bf2cd1bad
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-06-04 13:37:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package RandomNumberGame;
 import java.util.Scanner;
 
 public class Main {
-
+//edited by ashwini
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		while(true)
